32db6ca81bd16493017e9065838d5f2c05749b09
Francisco <francisco.barrientos@usach.cl>
2021-03-16 00:27:38 -0300

    Se agregan funcionalidades a las vistas

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -12,10 +18,45 @@ const events = [{
     title: 'Cumpleaños de Daniel',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel',
+    user: {
+        _id: '123',
+        name: 'Francisco'
+    }
 }]
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'month' );
+
+    const onDoubleClick = (e) => {
+        console.log(e)
+    }
+
+    const onSelectEvent = (e) => {
+        console.log(e)
+    }
+    
+    const onViewChange = (e) => {
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    }
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        console.log(event, start, end, isSelected);
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
     return (
         <div className="calendar-screen">
             <Navbar />
@@ -25,7 +66,18 @@ export const CalendarScreen = () => {
                 events={ events }
                 startAccessor="start"
                 endAccessor="end"
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
             />
+
+            <CalendarModal />
         </div>
     )
 }
